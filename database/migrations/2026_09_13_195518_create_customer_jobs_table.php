bd8a83c1b50fd498fa98833b92297ce1d2d82fad
tara-matthew <taramatthew.93@gmail.com>
2026-09-14 19:49:16 +0100

    Create migrations for customers, customer jobs, and appointments

---
+++ database/migrations/2026_09_13_195518_create_customer_jobs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Customer;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customer_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Customer::class)->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customer_jobs');
+    }
+};
